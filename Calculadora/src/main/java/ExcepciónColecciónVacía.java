2f2e3d9b9f70571f66c1dc7c2bc5365c43d66417
covaconv <rodrilee216@gmail.com>
2024-10-01 14:19:36 -0600

    agregue la pila

---
+++ Calculadora/src/main/java/ExcepciónColecciónVacía.java
@@ -0,0 +1,20 @@
+
+package conarreglos;
+
+/**
+ 
+ * Se define una clase propia para el manejo de excepción en los casos que la 
+ * colección o estructura de datos esté vacía.
+ */
+public class ExcepciónColecciónVacía extends RuntimeException{
+
+    public ExcepciónColecciónVacía(){
+        super("Colección vacía");
+    }
+
+    public ExcepciónColecciónVacía(String mensaje){
+        super(mensaje);
+    }
+}
+
+
