b9821bab65f12b51f4e0cc40e9c24ef81a6cc689
bargof <bargof@Bargofs-MacBook-Air.local>
2024-10-04 09:45:18 -0600

    comentarios javadoc de ExcepcionColeccionVacia

--- Calculadora/src/main/java/ExcepciónColecciónVacía.java
+++ Calculadora/src/main/java/ExcepciónColecciónVacía.java
@@ -1,14 +1,24 @@
 /**
- 
+ * Excepcion Colección Vacía
+
  * Se define una clase propia para el manejo de excepción en los casos que la 
  * colección o estructura de datos esté vacía.
+ * @author Ceci Leon, Aniel Orihuela, Rodrigo Flores, Fernando Barrios
+ * @version 1.1
  */
 public class ExcepciónColecciónVacía extends RuntimeException{
 
+    /**
+     * Constructor de excepcion con clase padre: RuntimeException
+     */
     public ExcepciónColecciónVacía(){
         super("Colección vacía");
     }
 
+    /**
+     * Raise exception 
+     * @param mensaje // Mensaje de error
+     */
     public ExcepciónColecciónVacía(String mensaje){
         super(mensaje);
     }
