70d4fb943fb6033708e54546460fae10a0d0da47
covaconv <rodrilee216@gmail.com>
2024-10-15 08:17:16 -0600

    edité el pom

--- Calculadora/src/main/java/CalculadoraTest.java
+++ Calculadora/src/main/java/CalculadoraTest.java
@@ -1,4 +1,6 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Test;
 
 public class CalculadoraTest {
